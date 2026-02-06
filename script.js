24a56234278023cf14b7bc9d06247e4896ddd1e0
MoselleCallo <mosellerebekhah@gmail.com>
2026-02-06 15:55:46 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function WebHeader({ cart, isOpen, setIsOpen }) {
+function WebHeader({ setCart, cart, isOpen, setIsOpen }) {
     return (
         <header className="fixed w-full z-50 bg-white">
             <div className="flex items-center justify-between w-full h-16 p-[25px] md:h-28 md:px-24">
@@ -10,7 +10,7 @@ function WebHeader({ cart, isOpen, setIsOpen }) {
 
                 {/* Cart and User*/}
                 <div className="flex justify-end gap-3 items-center">
-                    <CartIcon cart={cart} isOpen={isOpen} setIsOpen={setIsOpen} />
+                    <CartIcon setCart={setCart} cart={cart} isOpen={isOpen} setIsOpen={setIsOpen} />
                     <img className="h-8 w-8" alt="user" src="/images/image-avatar.png" />
                 </div>
             </div>
@@ -25,11 +25,13 @@ function WebHeader({ cart, isOpen, setIsOpen }) {
 
 function ToggleMenu() {
     const [isOpen, setIsOpen] = React.useState(false);
-
+    const [isBold, setIsBold] = React.useState("Collections");
+    const menus = ["Collections", "Mens", "Women", "About", "Contact"];
+    
     function toggle() {
         setIsOpen(!isOpen);
     }
-
+    
     return (
         <>
             <button className="z-50 md:hidden cursor-pointer"
@@ -44,35 +46,31 @@ function ToggleMenu() {
             </button>
 
             <ul className={`absolute h-screen w-2/3 top-0 left-0 pt-16 px-[25px] bg-white ${isOpen ? "block" : "hidden"}`}>
-                <li className="py-4 text-gray-900 font-bold">
-                    <a href="/"> Collections </a>
+                {menus.map(menu => (<li key={menu} onClick={() => setIsBold(menu)} className={`py-4 cursor-pointer text-gray-900 ${isBold === menu ? "font-bold" : "font-normal"}`}>
+                {menu}
                 </li>
-                <li onClick={() => setIsBold(!isBold)} className={`py-4 text-gray-900 ${isBold ? "font-bold" : "font-normal"}`}>
-                    <a href="/"> Men </a>
-                </li>
-                <li className="py-4 text-gray-900">
-                    <a href="/"> Women </a>
-                </li>
-                <li className="py-4 text-gray-900">
-                    <a href="/"> About </a>
-                </li>
-                <li className="py-4 text-gray-900">
-                    <a href="/"> Contact </a>
-                </li>
+                ))}
             </ul>
         </>
     );
 }
 
-function CartIcon({ cart, isOpen, setIsOpen }) {
-
+function CartIcon({ setCart, cart, isOpen, setIsOpen }) {
     function toggle() {
         setIsOpen(!isOpen);
     }
-
+    
+    function deleteIcon(id) {
+        setCart(prev => prev.filter(item => item.id !== id));
+    }
+    
+    const total = cart.reduce((sum, item) => sum + item.qty, 0);
+    
     return (
         <>
-            <button className="z-50 cursor-pointer" onClick={toggle}>
+            <button className="relative z-50 cursor-pointer" onClick={toggle}>
+                
+                <span className={`absolute font-bold text-white text-[10px] px-2 rounded-full bg-orange-500 -top-2 -right-2 ${cart.length > 0 ? "block" : "hidden"}`}>{total}</span>
                 <img alt="cart" src="/images/icon-cart.svg" />
             </button>
 
@@ -94,7 +92,7 @@ function CartIcon({ cart, isOpen, setIsOpen }) {
 
                     (cart.map((item) => (
                         <div className="p-4 h-44">
-                            <div className="flex items-center gap-2">
+                            <div key={item.id} className="flex items-center gap-4">
                                 <img className="h-12 w-12 rounded-md" src={item.src} alt={item.alt} key={item.id} />
 
                                 <div className="">
@@ -106,18 +104,19 @@ function CartIcon({ cart, isOpen, setIsOpen }) {
                                     </p>
                                 </div>
 
-                                <img className="items-center" src="/images/icon-delete.svg" />
+                 <button className="z-50 cursor-pointer" onClick={() => deleteIcon(item.id)}>
+                     <img className="items-center" alt="delete" src="/images/icon-delete.svg" />
+                 </button>               
                             </div>
-                        </div>
-                    ))
-                    )}
-
-                {cart.length != 0 &&
-                    (<button onClick={() => setIsClicked(!isClicked)} className="mt-4 mb-20 w-full flex items-center justify-center bg-orange-500 font-bold p-4 rounded-md hover:opacity-80 transition-opacity duration-200"
+                            {cart.length != 0 &&
+                    (<button onClick={() => setIsClicked(!isClicked)} className="mt-8 w-full flex items-center justify-center bg-orange-500 font-bold p-4 rounded-md hover:opacity-80 active:opacity-70 active:scale-95 transition-all duration-200"
                     >
                         Checkout
                     </button>)
                 }
+                        </div>
+                    ))
+                    )}
             </div >
         </>
     );
@@ -129,26 +128,26 @@ function Gallery() {
         { src: '/images/image-product-2.jpg', alt: 'product image' },
         { src: '/images/image-product-3.jpg', alt: 'product image' },
     ];
-
+    
     const [index, setIndex] = React.useState(0);
-
+    
     let hasPrev = index > 0;
     let hasNext = index < myImages.length - 1;
-
+    
     function imgPrev() {
         if (hasPrev) {
             setIndex(index - 1);
         }
     }
-
+    
     function imgNext() {
         if (hasNext) {
             setIndex(index + 1);
         }
     }
-
+    
     let myImg = myImages[index];
-
+    
     return (
         <>
             <div className="relative items-center">
@@ -192,12 +191,11 @@ function ProductDesc() {
     );
 }
 
-function ImgThumbnails() {
-}
+function ImgThumbnails() {}
 
 function AddToCart({ cart, setCart }) { // ADDING ITEMS
     const [count, setCount] = React.useState(0);
-
+    
     const product = {
         id: 1,
         name: 'Fall Limited  Edition Sneakers',
@@ -206,31 +204,31 @@ function AddToCart({ cart, setCart }) { // ADDING ITEMS
         src: '/images/image-product-1.jpg',
         alt: 'Product Image'
     };
-
+    
     let hasMinus = count > 0;
-
+    
     function minus() {
         if (hasMinus) {
             setCount(count - 1);
         }
     }
-
+    
     function plus() {
         setCount(count + 1);
     }
-
+    
     function add() {
         if (count === 0) return;
-
+        
         const existing = (cart.find(item => item.id === product.id));
-
+        
         if (existing) {
             setCart(prevCart => prevCart.map(item => item.id === product.id ? { ...item, qty: item.qty + count } : item));
         }
         else
             setCart(prev => [...prev, { ...product, qty: count }]);
     }
-
+    
     return (
         <div className="mx-[25px]">
             <div className="mt-7 px-5 py-4 bg-gray-50 flex justify-between items-center rounded-md">
@@ -245,8 +243,8 @@ function AddToCart({ cart, setCart }) { // ADDING ITEMS
                 </button>
             </div>
 
-            <button className="mt-4 mb-20 w-full flex items-center justify-center gap-3 bg-orange-500 font-bold p-4 rounded-md shadow-xl shadow-orange-500/50"
-                onClick={() => add(product)}
+            <button className="mt-4 mb-20 w-full flex items-center justify-center gap-3 bg-orange-500 font-bold p-4 rounded-md shadow-xl shadow-orange-500/50 hover:opacity-80 active:opacity-70 active:scale-95 transition-all duration-200"
+                onClick={() => {add(product); setCount(0);}}
             >
                 <img src="/images/icon-cart.svg" alt="Cart" className="w-4 h-4" />
                 <span>Add to cart</span>
@@ -271,11 +269,11 @@ function WebFooter() {
 function App() {
     const [cart, setCart] = React.useState([]);
     const [isOpen, setIsOpen] = React.useState(false);
-
-
+    
+    
     return (
         <>
-            <WebHeader cart={cart} isOpen={isOpen} setIsOpen={setIsOpen} />
+            <WebHeader setCart={setCart} cart={cart} isOpen={isOpen} setIsOpen={setIsOpen} />
 
             <div className="md:px-24">
                 <main className="pt-8 md:pt-28">
