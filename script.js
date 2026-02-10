3680bb2d3706a22524554ac183e246b122eb66a6
MoselleCallo <mosellerebekhah@gmail.com>
2026-02-10 16:46:57 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function WebHeader({ setCart, cart, isOpen, setIsOpen }) {
     return (
         <header className="fixed w-full z-50 bg-white">
-            <div className="flex items-center justify-between w-full h-16 p-[25px] md:h-28 md:px-24">
+            <div className="flex items-center justify-between w-full h-16 p-[25px] md:h-28 px-24">
                 <div className="flex gap-3 items-center">
                     {/* Toggle Menu */}
-                    <ToggleMenu />
                     <img className="h-5" alt="logo" src="/images/logo.svg" />
+                    <ToggleMenu />
                 </div>
 
                 {/* Cart and User*/}
-                <div className="flex justify-end gap-3 items-center">
+                <div className="flex justify-end gap-3 items-center md:gap-8">
                     <CartIcon setCart={setCart} cart={cart} isOpen={isOpen} setIsOpen={setIsOpen} />
                     <img className="h-8 w-8" alt="user" src="/images/image-avatar.png" />
                 </div>
             </div>
 
             {/* HR Line */}
-            <div className="hidden md:block px-24 mb-20">
+            <div className="hidden md:block px-24">
                 <hr className="border-t-1 border-gray-400" />
             </div>
         </header>
@@ -45,7 +45,9 @@ function ToggleMenu() {
                         "/images/icon-menu.svg"} />
             </button>
 
-            <ul className={`absolute h-screen w-2/3 top-0 left-0 pt-16 px-[25px] bg-white ${isOpen ? "block" : "hidden"}`}>
+            <ul className={`absolute h-screen w-2/3 top-0 left-0 pt-16 px-[25px] bg-white  ${isOpen ? "block" : "hidden"} 
+            
+            md:static md:h-auto md:w-auto md:pt-0 md:flex md:gap-6`}>
                 {menus.map(menu => (<li key={menu} onClick={() => setIsBold(menu)} className={`py-4 cursor-pointer text-gray-900 ${isBold === menu ? "font-bold" : "font-normal"}`}>
                 {menu}
                 </li>
@@ -151,7 +153,7 @@ function Gallery() {
     return (
         <>
             <div className="relative items-center">
-                <div className="w-full absolute p-5 flex justify-between top-1/2 -translate-y-1/2">
+                <div className="w-full absolute p-5 flex justify-between top-1/2 -translate-y-1/2 md:hidden">
                     <button className="z-50" onClick={imgPrev} disabled={!hasPrev}>
                         <img className="bg-white rounded-full p-3" alt="previous" src="/images/icon-previous.svg" />
                     </button>
@@ -161,7 +163,7 @@ function Gallery() {
                     </button>
                 </div>
 
-                <img alt={myImg.alt} src={myImg.src} />
+                <img className="rounded-xl" alt={myImg.alt} src={myImg.src} />
             </div>
         </>
     );
@@ -178,7 +180,7 @@ function ProductDesc() {
                         durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
                 </div>
 
-                <div className="flex my-7 items-center justify-between">
+                <div className="flex my-7 items-center justify-between md:block">
                     <div className="flex items-center gap-3">
                         <h2 className="text-3xl font-bold text-gray-900">$125.00</h2>
                         <p className="px-2 bg-gray-900 font-bold text-white rounded-md">50%</p>
@@ -230,8 +232,8 @@ function AddToCart({ cart, setCart }) { // ADDING ITEMS
     }
     
     return (
-        <div className="mx-[25px]">
-            <div className="mt-7 px-5 py-4 bg-gray-50 flex justify-between items-center rounded-md">
+        <div className="mx-[25px] mb-20 md:flex md:gap-4">
+            <div className="w-full px-5 py-4 bg-gray-50 flex justify-between items-center rounded-md md:w-1/3">
                 <button className="" onClick={minus} disabled={!hasMinus}>
                     <img src="/images/icon-minus.svg" alt="minus" className="" />
                 </button>
@@ -243,7 +245,7 @@ function AddToCart({ cart, setCart }) { // ADDING ITEMS
                 </button>
             </div>
 
-            <button className="mt-4 mb-20 w-full flex items-center justify-center gap-3 bg-orange-500 font-bold p-4 rounded-md shadow-xl shadow-orange-500/50 hover:opacity-80 active:opacity-70 active:scale-95 transition-all duration-200"
+            <button className="mt-4 w-full flex items-center justify-center gap-3 bg-orange-500 font-bold p-4 rounded-md shadow-xl shadow-orange-500/50 hover:opacity-80 active:opacity-70 active:scale-95 transition-all duration-200 md:w-2/3 md:m-0"
                 onClick={() => {add(product); setCount(0);}}
             >
                 <img src="/images/icon-cart.svg" alt="Cart" className="w-4 h-4" />
@@ -277,9 +279,14 @@ function App() {
 
             <div className="md:px-24">
                 <main className="pt-8 md:pt-28">
+                    <div className="md:flex md:gap-12 md:px-12 mt-20">
                     <Gallery />
-                    <ProductDesc />
-                    <AddToCart cart={cart} setCart={setCart} />
+                    
+                    <div>
+                     <ProductDesc />
+                                    <AddToCart cart={cart} setCart={setCart} />
+                    </div>
+                </div>
                 </main>
             </div>
 
